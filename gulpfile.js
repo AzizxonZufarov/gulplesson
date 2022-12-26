658549a9d099f34cc3c2c5c392efe75a25ca3933
AzizxonZufarov <bionorica2015@mail.ru>
2022-12-26 21:30:00 +0500

    last - only no menu burger

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-let project_folder = "dist";
+//let project_folder = "dist";
+let project_folder = require("path").basename(__dirname);
 let source_folder = "src";
+
+let fs = require('fs');
 
 let path =
 {
@@ -169,11 +172,38 @@ function watchFiles(params) {
     gulp.watch([path.watch.css], css);
     gulp.watch([path.watch.js], js);
     gulp.watch([path.watch.img], images);
+
 }
 
-let build = gulp.series(gulp.parallel(js, css, html, images, fonts));
+function fontsStyle(params) {
+
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {
+
+}
+
+let build = gulp.series(gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
